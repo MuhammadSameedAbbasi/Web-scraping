f19cd66b279ad7fb7bb5cf16049db5efb4478caf
Sameed Abbasi <msameedabbasi@gmail.com>
2023-12-18 22:43:31 +0500

    looped through pages

--- Webscrape cargoreps.py
+++ Webscrape cargoreps.py
@@ -2,104 +2,80 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Define the URL of the page
-url = "https://www.yellowpages-uae.com/uae/freight-forwarders"
-
-# Send a GET request and get the response
-response = requests.get(url)
-
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
-outputfile = open("G:\Projects VS\webScraping\outputfilecarge.txt", "a")
-# Find all the div elements that have the class "company-details"
-divs = soup.find_all("div", class_="row box premium")
-
-# Loop through each div element
-for div in divs:
-    # Find the h2 element that contains the name of the company
-    h2 = div.find("h2",class_="cmp_name")
-    # Get the text of the h2 element and strip any whitespace
-    name = h2.text.strip()
-    outputfile.write(name+"||")
-    # Find the span element that contains the address of the company
-    span = div.find("span", class_="location")
-    # Get the text of the span element and strip any whitespace
-    address = span.text.strip()
-    outputfile.write(address+"||")
-    
-    #city
-    spancity = div.find("span", class_="locationCity")
-    # Get the text of the span element and strip any whitespace
-    address = spancity.text.strip()
-    outputfile.write(address+"||")
-
-    # Find the span element that contains the phone number of the company
-    span = div.find("span", class_="phone")
-    # Get the text of the span element and strip any whitespace
-    phone = span.text.strip()
-    # Print the name, address, and phone number of the company
-    outputfile.write(phone)
-    outputfile.write("\n")
 
 
+def readdata(divs):
+    outputfile = open("G:\Projects VS\webScraping\outputfilecarge.txt", "a")
+    # Loop through each div element
+    for div in divs:
+        try:
+            # Find the h2 element that contains the name of the company
+            h2 = div.find("h2",class_="cmp_name")
+            # Get the text of the h2 element and strip any whitespace
+            name = h2.text.strip()
+            outputfile.write(name+"|")
+        except:
+            pass
+        try:
+            # Find the span element that contains the address of the company
+            span = div.find("span", class_="location")
+            # Get the text of the span element and strip any whitespace
+            address = span.text.strip()
+            outputfile.write(address+"|")
+        except:
+            pass
 
-divs2 = soup.find_all("div", class_="row box regular")
+        try:
+            #city
+            spancity = div.find("span", class_="locationCity")
+            # Get the text of the span element and strip any whitespace
+            address = spancity.text.strip()
+            outputfile.write(address+"|")
+        except:
+            pass
 
-# Loop through each div element
-for div in divs2:
-    # Find the h2 element that contains the name of the company
-    h2 = div.find("h2",class_="cmp_name")
-    # Get the text of the h2 element and strip any whitespace
-    name = h2.text.strip()
-    outputfile.write(name+"||")
-    # Find the span element that contains the address of the company
-    span = div.find("span", class_="location")
-    # Get the text of the span element and strip any whitespace
-    address = span.text.strip()
-    outputfile.write(address+"||")
-    
-    #city
-    spancity = div.find("span", class_="locationCity")
-    # Get the text of the span element and strip any whitespace
-    address = spancity.text.strip()
-    outputfile.write(address+"||")
+        try:
+            # Find the span element that contains the phone number of the company
+            span = div.find("span", class_="phone")
+            # Get the text of the span element and strip any whitespace
+            phone = span.text.strip()
+            # Print the name, address, and phone number of the company
+            outputfile.write(phone)
+        except:
+            pass
+        outputfile.write("\n")
 
-    # Find the span element that contains the phone number of the company
-    span = div.find("span", class_="phone")
-    # Get the text of the span element and strip any whitespace
-    phone = span.text.strip()
-    # Print the name, address, and phone number of the company
-    outputfile.write(phone)
-    outputfile.write("\n")
 
-divs2 = soup.find_all("div", class_="row box foc")
 
-# Loop through each div element
-for div in divs2:
-    # Find the h2 element that contains the name of the company
-    h2 = div.find("h2",class_="cmp_name")
-    # Get the text of the h2 element and strip any whitespace
-    name = h2.text.strip()
-    outputfile.write(name+"||")
-    try:
-        # Find the span element that contains the address of the company
-        span = div.find("span", class_="location")
-        # Get the text of the span element and strip any whitespace
-        address = span.text.strip()
-        outputfile.write(address+"||")
-    except:
-        pass
-    
-    #city
-    spancity = div.find("span", class_="locationCity")
-    # Get the text of the span element and strip any whitespace
-    address = spancity.text.strip()
-    outputfile.write(address+"||")
+def box_call(soup):
+    divs = soup.find_all("div", class_="row box premium")
+    readdata(divs)
 
-    # Find the span element that contains the phone number of the company
-    span = div.find("span", class_="phone")
-    # Get the text of the span element and strip any whitespace
-    phone = span.text.strip()
-    # Print the name, address, and phone number of the company
-    outputfile.write(phone)
-    outputfile.write("\n")
+    divs = soup.find_all("div", class_="row box regular")
+    readdata(divs)
+
+    divs = soup.find_all("div", class_="row box foc")
+    readdata(divs)
+
+def page_call():
+    # Define the URL of the page
+    url = "https://www.yellowpages-uae.com/uae/freight-forwarders"
+
+    # Send a GET request and get the response
+    # response = requests.get(url)
+
+    # # Parse the HTML content using BeautifulSoup
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # box_call(soup)
+
+    for i in range(20,21):
+        url += "-" + str(i) + ".html"
+        # Send a GET request and get the response
+        response = requests.get(url)
+
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
+        box_call(soup)
+
+
+page_call()
